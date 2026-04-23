fd83f1d196fa5c8c1bc6fc6d2981459bcaebb576
BlueDog31 <martsimu1@gmail.com>
2026-04-23 09:49:31 +0200

    Add data handling for mean and standard deviation

--- data handling.py
+++ data handling.py
@@ -1,1 +1,10 @@
 
+from pylab import *
+
+y = array([3.73, 3.53, 3.75, 3.63, 3.82])
+n = len(y)
+y_snitt = sum(y) / n
+s = sqrt(sum(y - y_snitt)**2 / (n - 1))
+
+print("Gjennomsnittet er:", round(y_snitt, 2))
+print("standardavviket er", round(s, 2))
