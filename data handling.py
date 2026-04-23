a0637776a4d3f4b95c2ed506eb306dc63ec218ab
BlueDog31 <martsimu1@gmail.com>
2026-04-23 09:51:58 +0200

    Calculate standard error and error margin

--- data handling.py
+++ data handling.py
@@ -8,3 +8,6 @@ s = sqrt(sum(y - y_snitt)**2 / (n - 1))
 
 print("Gjennomsnittet er:", round(y_snitt, 2))
 print("standardavviket er", round(s, 2))
+
+se = s/sqrt(n) #se = standard error
+em = 2*se #em = error margin/feilmargin
