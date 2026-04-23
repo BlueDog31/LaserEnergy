6c51c67f3fe67b972d1066bc9e310f96855e3711
BlueDog31 <martsimu1@gmail.com>
2026-04-23 09:53:26 +0200

    Update data handling.py

--- data handling.py
+++ data handling.py
@@ -5,9 +5,9 @@ y = array([3.73, 3.53, 3.75, 3.63, 3.82])
 n = len(y)
 y_snitt = sum(y) / n
 s = sqrt(sum(y - y_snitt)**2 / (n - 1))
-
-print("Gjennomsnittet er:", round(y_snitt, 2))
-print("standardavviket er", round(s, 2))
-
 se = s/sqrt(n) #se = standard error
 em = 2*se #em = error margin/feilmargin
+
+print("Gjennomsnittet er:", round(y_snitt, 2))
+print("Standardavviket er", round(s, 2))
+print("Standard error is:", round(se, 2))
